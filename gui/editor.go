9dfe77b16b6a60cb594078a73933ec50002a30ac
luhrMan <chris@luhrman.net>
2024-11-27 22:40:04 -0800

    add: wait settings bindings

---
+++ gui/editor.go
@@ -0,0 +1,14 @@
+package gui
+
+import (
+	"Dark-And-Darker/structs"
+
+	"fyne.io/fyne/v2/data/binding"
+)
+
+var (
+	actionBindingWait = 
+	actionBindingMouseMove binding.Struct
+	actionBindingClick binding.Struct
+	
+)
