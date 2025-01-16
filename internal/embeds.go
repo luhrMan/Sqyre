a06cbd9dade727b4f1152d257bd42dc9a2732e59
luhrMan <chris@luhrman.net>
2025-01-15 22:38:10 -0800

    add: subtraction masks and corner masks for item sizes

--- internal/embeds.go
+++ internal/embeds.go
@@ -66,3 +66,60 @@ func CreateItemMaps() {
                 return
         }
 }
+
+//func MaskItems() *map[string][]byte {
+//        //        icons = *GetIconBytes()
+//        maskedIcons := make(map[string][]byte)
+//        Imask := gocv.IMRead("./internal/resources/images/empty-stash.png", gocv.IMReadColor)
+//        Tmask := gocv.NewMat()
+//        defer Imask.Close()
+//        defer Tmask.Close()
+//        xSplit := 20
+//        ySplit := 12
+//        xSize := Imask.Cols() / ySplit
+//        ySize := Imask.Rows() / xSplit
+//
+//        for s, b := range icons {
+//                s = strings.TrimSuffix(s, filepath.Ext(s))
+//                //                ip := s + ".png"
+//                //                b := icons[ip]
+//                t := gocv.NewMat()
+//                defer t.Close()
+//                err := gocv.IMDecodeIntoMat(b, gocv.IMReadColor, &t)
+//                if err != nil {
+//                        fmt.Println("Error reading template image:", s)
+//                        fmt.Println(err)
+//                        continue
+//                }
+//                i, _ := Items.GetItem(s)
+//                if i == nil {
+//                        log.Println("failed to load: ", s)
+//                        continue
+//                }
+//                switch i.GridSize {
+//                case [2]int{1, 1}:
+//                        Tmask = Imask.Region(image.Rect(0, 0, xSize, ySize))
+//                case [2]int{1, 2}:
+//                        Tmask = Imask.Region(image.Rect(0, 0, xSize, ySize*2))
+//                case [2]int{1, 3}:
+//                        Tmask = Imask.Region(image.Rect(0, 0, xSize, ySize*3))
+//                case [2]int{2, 1}:
+//                        Tmask = Imask.Region(image.Rect(0, 0, xSize*2, ySize))
+//                case [2]int{2, 2}:
+//                        Tmask = Imask.Region(image.Rect(0, 0, xSize*2, ySize*2))
+//                case [2]int{2, 3}:
+//                        Tmask = Imask.Region(image.Rect(0, 0, xSize*2, ySize*3))
+//                }
+//                if Tmask.Cols() != t.Cols() && Tmask.Rows() != t.Rows() {
+//                        log.Println("item:", s)
+//                        log.Println("Tmask cols:", Tmask.Cols())
+//                        log.Println("Tmask rows:", Tmask.Rows())
+//                        log.Println("t cols:", t.Cols())
+//                        log.Println("t rows:", t.Rows())
+//                        continue
+//                }
+//                gocv.Subtract(t, Tmask, &t)
+//                maskedIcons[s] = t.ToBytes()
+//        }
+//        return &maskedIcons
+//}
